ac389b64927f26ddbc5c424bbec1d5554ce17279
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-07 14:55:05 -0400

    Initial Commit

---
+++ AddAFriend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddAFriend
+//
+//  Created by Kunwardeep Gill on 2015-04-07.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddAFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddAFriend
+//
+//  Created by Kunwardeep Gill on 2015-04-07.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
